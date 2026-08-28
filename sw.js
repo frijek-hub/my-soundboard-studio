3de647509e43aaa7a5134f0cd52a49973eb594b2
frijek-hub <frijek@web.de>
2026-08-28 12:22:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,78 @@
-const CACHE="msb-v1";
+const CACHE = "msb-v2";
 
-self.addEventListener("install",event=>{
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+
+/* =========================================================
+   INSTALL
+========================================================= */
+
+self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE).then(cache=>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.webmanifest"
-      ])
-    )
+
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+
   );
+
+  /* Neue Version sofort aktivieren */
+  self.skipWaiting();
+
 });
 
-self.addEventListener("fetch",event=>{
-  event.respondWith(
-    caches.match(event.request).then(response=>
-      response || fetch(event.request)
-    )
+
+/* =========================================================
+   AKTIVIEREN
+   Alte Caches löschen
+========================================================= */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys()
+      .then(keys => {
+
+        return Promise.all(
+
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+
+        );
+
+      })
+      .then(() => self.clients.claim())
+
   );
+
+});
+
+
+/* =========================================================
+   DATEIEN LADEN
+========================================================= */
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request);
+
+      })
+
+  );
+
 });
